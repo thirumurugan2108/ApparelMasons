984bce938a76216ee28771bb7a6834a0ab4d3154
keyil786 <keyildeverse786@gmailcom>
2020-04-03 21:05:40 +0530

    keyle coomit

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -5,11 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.css']
 })
-export class AboutUsComponent implements OnInit {
+export class AboutUsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+ 
 
 }
